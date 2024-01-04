0abea3a2d6f2451615454f46ec3eaac56c93daf2
trung <leqtrungggg3105@gmail.com>
2024-01-04 10:20:28 +0700

    setup app and icon

---
+++ appNavigation/AppNavigation.js
@@ -0,0 +1,17 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "../Screens/Home";
+
+const Stack = createNativeStackNavigator();
+
+function AppNavigation() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default AppNavigation
