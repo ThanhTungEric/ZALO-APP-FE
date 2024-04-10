418f7af494f198dc6a3f313f59adafff99c3d183
ThanhTungEric <99382836+ThanhTungEric@users.noreply.github.com>
2024-04-10 20:14:39 +0700

    merge code

--- router/APIRouter.js
+++ router/APIRouter.js
@@ -1,4 +1,4 @@
-export const host = "http://172.20.10.5:8080";
+export const host = "http://192.168.1.6:8080";
 export const login = `${host}/user/login`;
 export const register = `${host}/user/`;
 export const forgotPassword = `${host}/user/forgotPassword`;
