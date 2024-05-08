fbad9aec7ee373ac1944b013591ac1678b91e161
Vo-Hoang-Minh-Sang <126309748+Vo-Hoang-Minh-Sang@users.noreply.github.com>
2024-05-08 14:54:06 +0700

    update Messages

--- Screens/messages/Messages.js
+++ Screens/messages/Messages.js
@@ -1,4 +1,3 @@
-// Import các modules và hooks cần thiết
 import React, { useState, useEffect, useRef } from 'react';
 import { StyleSheet, Text, View, ScrollView, TextInput, TouchableOpacity, Image, FlatList, } from 'react-native';
 import { AntDesign, Ionicons, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons';
@@ -9,7 +8,7 @@ import PageContainer from '../../Components/PageContainer'
 import { COLORS, FONTS } from '../../constrants/theme'
 
 import AsyncStorage from '@react-native-async-storage/async-storage'
-import { getFriendListRoute, host } from '../../router/APIRouter';
+import { getFriendListRoute, host, getAllGroupByMemberId } from '../../router/APIRouter'; // Combine API imports
 import { io } from "socket.io-client";
 
 
@@ -18,34 +17,33 @@ const Messages = () => {
   const navigation = useNavigation();
   const [user, setUser] = useState(null);
   const [data, setData] = useState([]);
+  const [groups, setGroups] = useState([]);
   const socket = useRef();
-
-  const getUser = async () => {
-    try {
-      const value = await AsyncStorage.getItem('userData');
-      if (value !== null) {
-        const parsUser = JSON.parse(value);
-        setUser(parsUser);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  useEffect(() => {
-    getUser();
-  }, []);
-
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        if (user && user._id) { // Check if user is not null and has id property
+        const value = await AsyncStorage.getItem('userData');
+        if (value !== null) {
+          const parsUser = JSON.parse(value);
+          setUser(parsUser);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const fetchFriendList = async () => {
+      try {
+        if (user && user._id) {
           const response = await fetch(`${getFriendListRoute}/${user._id}`);
           if (response.ok) {
             const data = await response.json();
             const friendInfos = data.map(item => item.friendInfo);
             setData(friendInfos);
-            console.log('finus', data);
           } else {
             console.error('Get friend list failed');
           }
@@ -54,7 +52,7 @@ const Messages = () => {
         console.error('Error:', error);
       }
     };
-    fetchData();
+    fetchFriendList();
   }, [user]);
 
   useEffect(() => {
@@ -64,6 +62,28 @@ const Messages = () => {
     }
   }, [user]);
 
+  useEffect(() => {
+    const fetchGroups = async () => {
+      try {
+        if (user && user._id) {
+          const response = await fetch(`${getAllGroupByMemberId}/${user._id}`, {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          const data = await response.json();
+          setGroups(data);
+        }
+      } catch (error) {
+        console.error("Error fetching group data:", error);
+      }
+    };
+    fetchGroups();
+  }, [user]);
 
   const handlePress = (item) => {
     navigation.navigate('ChatBox', { selectedChat: item, socket });
@@ -73,7 +93,6 @@ const Messages = () => {
   const [search, setSearch] = useState('')
   const [filteredData, setFilteredData] = useState([])
 
-  // Tìm kiếm dữ liệu
   useEffect(() => {
     const filtered = data.filter((item) =>
       item.fullName.toLowerCase().includes(search.toLowerCase())
@@ -81,62 +100,61 @@ const Messages = () => {
     setFilteredData(filtered);
   }, [search, data]);
 
-  console.log("đây là data", data)
-
   return (
     <SafeAreaView>
       <PageContainer>
-        <View >
-          <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginHorizontal: 22, marginTop: 22, }}>
-            <Text style={{ ...FONTS.h4 }}>Tin nhắn </Text>
-            <View style={{ flexDirection: 'row' }}>
-              <MaterialCommunityIcons name="message-badge-outline" size={20} color={COLORS.secondaryBlack} />
-              <MaterialCommunityIcons name="playlist-check" size={20} color={COLORS.secondaryBlack} style={{ marginLeft: 12, }} />
-            </View>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginHorizontal: 22, marginTop: 22 }}>
+          <Text style={{ ...FONTS.h4 }}>Tin nhắn </Text>
+          <View style={{ flexDirection: 'row' }}>
+            <MaterialCommunityIcons name="message-badge-outline" size={20} color={COLORS.secondaryBlack} />
+            <MaterialCommunityIcons name="playlist-check" size={20} color={COLORS.secondaryBlack} style={{ marginLeft: 12 }} />
           </View>
-          <View style={{ marginHorizontal: 22, flexDirection: 'row', alignItems: 'center', }}>
-            <View style={{ flexDirection: 'column', alignItems: 'center', marginRight: 4, }}>
-              <TouchableOpacity style={{ height: 50, width: 50, borderRadius: 25, alignItems: 'center', justifyContent: 'center', backgroundColor: '#e6edff', marginBottom: 4, }}>
-                <AntDesign name="plus" size={24} color={COLORS.black} />
-              </TouchableOpacity>
-            </View>
-            <View>
-              <FlatList
-                data={data}
-                horizontal={true}
-                renderItem={({ item, index }) => (
-                  <View style={{ flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-                    <TouchableOpacity style={{ paddingVertical: 15, marginRight: 22 }}>
-                      <Image source={{ uri: item.avatar }} resizeMode="contain" style={{ height: 50, width: 50, borderRadius: 25 }} />
-                    </TouchableOpacity>
-                    <Text>{item.fullName.substring(0, 5)}...</Text>
-                  </View>
-                )}
-                keyExtractor={(item, index) => index.toString()}
-              />
-            </View>
-
+        </View>
+        <View style={{ marginHorizontal: 22, flexDirection: 'row', alignItems: 'center' }}>
+          <View style={{ flexDirection: 'column', alignItems: 'center', marginRight: 4 }}>
+            <TouchableOpacity style={{ height: 50, width: 50, borderRadius: 25, alignItems: 'center', justifyContent: 'center', backgroundColor: '#e6edff', marginBottom: 4 }}>
+              <AntDesign name="plus" size={24} color={COLORS.black} />
+            </TouchableOpacity>
           </View>
-          <View style={{
-            marginHorizontal: 22,
-            flexDirection: 'row',
-            alignItems: 'center',
-            backgroundColor: '#574E92',
-            height: 48,
-            marginVertical: 22,
-            paddingHorizontal: 12,
-            borderRadius: 20,
-          }}
-          >
-            <Ionicons name="search-outline" size={24} color={COLORS.white} />
-            <TextInput style={{ width: '100%', height: '100%', marginHorizontal: 12, color: '#fff', }}
-               onChangeText={(text) => setSearch(text)}
-              value={search}
-              placeholder="Tìm kiếm tên..."
-              placeholderTextColor={'#fff'}
+          <View>
+            <FlatList
+              data={data}
+              horizontal={true}
+              renderItem={({ item, index }) => (
+                <View style={{ flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+                  <TouchableOpacity style={{ paddingVertical: 15, marginRight: 22 }}>
+                    <Image source={{ uri: item.avatar }} resizeMode="contain" style={{ height: 50, width: 50, borderRadius: 25 }} />
+                  </TouchableOpacity>
+                  <Text>{item.fullName.substring(0, 5)}...</Text>
+                </View>
+              )}
+              keyExtractor={(item, index) => index.toString()}
             />
           </View>
-          <View style={{ paddingBottom: 100, backgroundColor: '#fff', marginTop: 10 }}>
+        </View>
+        <View style={{
+          marginHorizontal: 22,
+          flexDirection: 'row',
+          alignItems: 'center',
+          backgroundColor: '#574E92',
+          height: 48,
+          marginVertical: 22,
+          paddingHorizontal: 12,
+          borderRadius: 20,
+        }}>
+          <Ionicons name="search-outline" size={24} color={COLORS.white} />
+          <TextInput style={{ width: '100%', height: '100%', marginHorizontal: 12, color: '#fff' }}
+            onChangeText={(text) => setSearch(text)}
+            value={search}
+            placeholder="Tìm kiếm tên..."
+            placeholderTextColor={'#fff'}
+          />
+        </View>
+        <View style={{ height: '60%' }}>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginHorizontal: 22, marginTop: 22 }}>
+            <Text style={{ ...FONTS.h4 }}>Danh sách bạn bè </Text>
+          </View>
+          <ScrollView style={{ paddingBottom: 100, backgroundColor: '#fff', marginTop: 10, height: '20%' }}>
             {data.map((item, index) => (
               <TouchableOpacity
                 key={index}
@@ -157,17 +175,17 @@ const Messages = () => {
                     : null,
                 ]}
               >
-                <View style={{ paddingVertical: 15, marginRight: 22, }}>
+                <View style={{ paddingVertical: 15, marginRight: 22 }}>
                   {/* {item.status === 'online' && (
-                    <View style={{ height: 14, width: 14, borderRadius: 7, backgroundColor: COLORS.green, borderColor: COLORS.white, borderWidth: 2, position: 'absolute', top: 14, right: 2, zIndex: 1000, }} />
+                    <View style={{ height: 14, width: 14, borderRadius: 7, backgroundColor: COLORS.green, borderColor: COLORS.white, borderWidth: 2, position: 'absolute', top: 14, right: 2, zIndex: 1000 }} />
                   )} */}
                   <Image
                     source={{ uri: item.avatar }}
                     resizeMode="contain"
-                    style={{ height: 50, width: 50, borderRadius: 25, }}
+                    style={{ height: 50, width: 50, borderRadius: 25 }}
                   />
                 </View>
-                <View style={{ flexDirection: 'column', }}>
+                <View style={{ flexDirection: 'column' }}>
                   <Text style={{ ...FONTS.h4, marginBottom: 4 }}>
                     {item.fullName}
                   </Text>
@@ -177,10 +195,36 @@ const Messages = () => {
                 </View>
               </TouchableOpacity>
             ))}
+          </ScrollView>
+
+
+
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginHorizontal: 22, marginTop: 22 }}>
+            <Text style={{ ...FONTS.h4 }}>Nhóm </Text>
           </View>
+          {/* Render danh sách nhóm */}
+          <ScrollView style={{ height: '20%' }}>
+            {groups.map((group, index) => (
+              <TouchableOpacity
+                key={index}
+                onPress={() => navigation.navigate('ChatGroup', { group, socket })}
+                style={[
+                  { width: '100%', flexDirection: 'row', alignItems: 'center', paddingHorizontal: 22, borderBottomColor: COLORS.secondaryWhite, borderBottomWidth: 1, },
+                  index % 2 !== 0 ? { backgroundColor: COLORS.tertiaryWhite } : null,
+                ]}
+              >
+                <View style={{ paddingVertical: 15, marginRight: 22, }}>
+                  <Image source={{ uri: group.avatar }} resizeMode="contain" style={{ height: 50, width: 50, borderRadius: 25, }} />
+                </View>
+                <View style={{ flexDirection: 'column', }}>
+                  <Text style={{ ...FONTS.h4, marginBottom: 4 }}>{group.groupName}</Text>
+                </View>
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
         </View>
       </PageContainer>
-    </SafeAreaView >
+    </SafeAreaView>
   )
 }
 
